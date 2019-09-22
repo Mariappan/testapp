a6136b26a10c17a8ddd8cba022ac126638ea8280
Mariappan R <rmariappan@outlook.com>
2019-09-22 20:23:52 +0530

    Updates

---
+++ precache-manifest.1e3666298898bbbc70d4e2013086cf59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f42b726ae54b3907e9211834af4a25",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "49dfc14917183556fd26",
+    "url": "/testapp/static/css/main.08d1e9e4.chunk.css"
+  },
+  {
+    "revision": "c55fca507bce91e7de3f",
+    "url": "/testapp/static/js/2.91f6b5ec.chunk.js"
+  },
+  {
+    "revision": "49dfc14917183556fd26",
+    "url": "/testapp/static/js/main.8f2de074.chunk.js"
+  },
+  {
+    "revision": "da941caed283e3e544ee",
+    "url": "/testapp/static/js/runtime~main.6dc883ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
