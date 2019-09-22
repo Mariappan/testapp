a55330111327fc5e0ced046eea9668b8ff543758
Mariappan R <rmariappan@outlook.com>
2019-09-22 20:48:30 +0530

    Updates

--- precache-manifest.ea50c7e61ae327a3c86a1be22465a580.js
+++ precache-manifest.ea50c7e61ae327a3c86a1be22465a580.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c3f42b726ae54b3907e9211834af4a25",
+    "revision": "28d408d0e56021e001ff4173593df8d7",
     "url": "/testapp/index.html"
   },
   {
-    "revision": "49dfc14917183556fd26",
+    "revision": "57b45badc254f24be442",
     "url": "/testapp/static/css/main.08d1e9e4.chunk.css"
   },
   {
-    "revision": "c55fca507bce91e7de3f",
-    "url": "/testapp/static/js/2.91f6b5ec.chunk.js"
+    "revision": "a957e5257f31706630e3",
+    "url": "/testapp/static/js/2.a1fb672e.chunk.js"
   },
   {
-    "revision": "49dfc14917183556fd26",
-    "url": "/testapp/static/js/main.8f2de074.chunk.js"
+    "revision": "57b45badc254f24be442",
+    "url": "/testapp/static/js/main.a07533b7.chunk.js"
   },
   {
     "revision": "da941caed283e3e544ee",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/testapp/precache-manifest.1e3666298898bbbc70d4e2013086cf59.js"
+  "/testapp/precache-manifest.ea50c7e61ae327a3c86a1be22465a580.js"
 );
 
 self.addEventListener('message', (event) => {
