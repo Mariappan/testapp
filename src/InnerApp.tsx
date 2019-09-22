6ede192baa29d7ac24b0eb6d43b8af7a838b652f
Mariappan R <rmariappan@outlook.com>
2019-09-22 20:51:12 +0530

    Create innerApp

---
+++ src/InnerApp.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import { RoughText } from './components/RoughText/RoughText';
+import { Clock } from './components/Clock';
+
+class InnerApp extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div className="App-main">
+          <RoughText />
+          <Clock />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default InnerApp;
